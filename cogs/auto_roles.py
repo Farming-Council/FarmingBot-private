7c7a1c9d13f884a9c9fa344fa5951c13054fba0f
Dyslexus <robertcirlanm@gmail.com>
2023-04-11 12:41:18 +0200

    Update auto_roles.py

--- cogs/auto_roles.py
+++ cogs/auto_roles.py
@@ -39,17 +39,17 @@ class autoroles(commands.Cog):
                 else:
                     uuid = await self.bot.get_uuid(ign)
                     profile = await self.bot.get_most_recent_profile(uuid)
-            weight = await calculate_farming_weight(self.bot,ign,profile)
-            if weight[1]["total"] == 0:
-                embed = discord.Embed(title="Error",description=weight[1])            
+            weight = await calculate_farming_weight(self.bot,uuid)
+            if weight == 0:
+                embed = discord.Embed(title="Error",description=weight)            
                 return
-            if weight[1]["total"] >= 1500:
+            if weight >= 1500:
                 embed = discord.Embed(title="You are eligible for **Certified Farmer**!", description = f"""Congratulations {interaction.user}\n\nThe role "Certified Farmer" should be added to you.""")
                 guild = interaction.guild
                 role = interaction.guild.get_role(1023315201875005520)
                 await interaction.user.add_roles(role)
             else:
-                embed = discord.Embed(title="You do not meet the requirements for certified farmer", description = f"""Sorry {interaction.user}\n\nYou need 2000+ farming weight to get the certified farmer role, and you currently have {round(weight[1]["total"], 2)}""", color=0x2F3136)
+                embed = discord.Embed(title="You do not meet the requirements for certified farmer", description = f"""Sorry {interaction.user}\n\nYou need 1500+ farming weight to get the certified farmer role, and you currently have {round(weight, 2)}""", color=0x2F3136)
                 embed.set_image(url='attachment://image.png')
                 embed.set_footer(text="Made by FarmingCouncil",
                             icon_url="https://i.imgur.com/4YXjLqq.png")
@@ -65,23 +65,24 @@ class autoroles(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def forceupdate(self, interaction: discord.Interaction):
         members = interaction.guild.members
-        channel =  self.bot.get_channel(1095276698477527131)
+        channel =  self.bot.get_channel(1095291940007845950)
         await interaction.response.send_message(f"Running through {len(members)} Members")
         for user in members:
             try:
                 ign = await self.bot.get_db_info(int(user.id))
+                ign = user.nick
+                if ign == None:
+                    ign = user.name
                 if ign != None:
                     uuid = await self.bot.get_uuid(ign)
-                    profile = await self.bot.get_most_recent_profile(uuid)
-                    weight = await calculate_farming_weight(self.bot,ign,profile)
+                    weight = await calculate_farming_weight(self.bot, uuid)
+                    await channel.send(f"```Discord Name: {user}\nDiscord ID: {user.id}\nMinecraft IGN: {ign}\nWeight: {round(weight, 2)}```")
                     role = interaction.guild.get_role(1023315201875005520)
-                    await channel.send(f"Discord Name: {user}\nDiscord ID: {user.id}\nMinecraft IGN: {ign}\nWeight: {round(weight[1]['total'], 2)}")
-                    if weight[1]["total"] >= 1500:
+                    if weight >= 1500:
                         await interaction.user.add_roles(role)
                     else:
                         await interaction.user.remove_roles(role)
             except Exception as e:
-                await channel.send(f"Error: {e}\nDiscord Name: {user}")
                 continue
                 
     
@@ -93,109 +94,14 @@ def try_it(member,collat):
     except:
         return 1
         
-async def calculate_farming_weight(self, ign,profile = ""):
+async def calculate_farming_weight(self, uuid):
     # Get profile and player data
-    async with self.session.get(f"https://slothpixel.farmingcouncil.com/api/skyblock/profile/{ign}/{profile}") as req:
+    async with self.session.get(f"https://elitebot.dev/api/weight/{uuid}") as req:
         try:
             response = await req.json()
         except Exception as e:
             return [0,"Hypixel is down"]
-    async with self.session.get(f"https://slothpixel.farmingcouncil.com/api/players/{ign}") as req:
-        try:
-            player = await req.json()
-        except Exception as e:
-            return [0,"Hypixel is down"]
-
-    json = response
-    try:
-        error = json["error"]
-        return [2,error]
-    except:
-        pass
-    try:
-        weight = 0
-        member = None
-        for i in json["members"]:
-            if json["members"][i]["uuid"] == player["uuid"]:
-                member = json["members"][i]
-    except:
-        pass
-    if member:
-        
-        try:
-            farming_level = int(member["skills"]["farming"]["level"])
-        except:
-            farming_level = 0
-        cactus = try_it(member,"CACTUS")/169389
-        carrot = try_it(member,"CARROT_ITEM")/300000
-        cocoa = try_it(member,"INK_SACK:3")/303092
-        melon = try_it(member,"MELON")/435466
-        mushroom = try_it(member,"MUSHROOM_COLLECTION")
-        wart = try_it(member,"NETHER_STALK")/250000
-        potato = try_it(member,"POTATO")/300000
-        pumpkin = try_it(member,"PUMPKIN")/87095
-        sugar = try_it(member,"SUGAR_CANE")/200000
-        wheat = try_it(member,"WHEAT")/100000
-        total= cactus + carrot + cocoa + melon + wart + potato + pumpkin + sugar + wheat
-
-        # Caculate weight for mushrooms dynamically
-        doubleBreakRatio = (cactus/169389 + sugar/200000) / total
-        normalRatio = (total - cactus/169389 - sugar/200000) / total
-        mushroomWeight = doubleBreakRatio * (mushroom / (2 * 168925.53)) + normalRatio * (mushroom / 168925.53)
-
-        weight +=mushroomWeight
-        weight += total
-
-        farming_weight = 0
-
-        if farming_level >= 60:
-            weight += 250
-            farming_weight += 250
-        elif farming_level >= 50:
-            weight += 100
-            farming_weight += 100
-        
-        minion_weight = 0
-        minions= []
-        for i in json["unlocked_minions"]:
-            if i in ["CACTUS","CARROT","COCOA","MELON","MUSHROOM","NETHER_WARTS","POTATO","PUMPKIN","SUGAR_CANE","WHEAT"]:
-                if json["unlocked_minions"][i] == 12:
-                    weight+=5
-                    minion_weight+=5
-                    minions.append(i)
-        jacub_weight = 0
-        jacub_perks = 0
-        try:
-            weight += member["jacob2"]["perks"]["double_drops"]*2
-            jacub_weight+=member["jacob2"]["perks"]["double_drops"]*2
-            jacub_perks+=member["jacob2"]["perks"]["double_drops"]
-
-        except:
-            pass
-
-        gold = 0
-        gold_weight = 0
-        for i in member["jacob2"]["contests"]:
-            try:
-                if member["jacob2"]["contests"][i]["claimed_medal"] == "gold":
-                    gold+=1
-            except:
-                try:
-                    if member["jacob2"]["contests"][i]["claimed_position"]<=member["jacob2"]["contests"][i]["claimed_participants"] * 0.05 + 1:
-                        gold+=1
-                except:
-                    pass
-                pass
-        if gold >=1000:
-            weight += 500
-            gold_weight += 500
-        else:
-            weight += gold*0.50
-            gold_weight += gold*0.50
-        
-        return [1,{"profile":profile,"total":weight,"collection_total":{"total":total+mushroomWeight,"cactus":cactus,"carrot":carrot,"cocoa":cocoa,"melon":melon,"wart":wart,"potato":potato,"pumpkin":pumpkin,"sugar":sugar,"wheat":wheat,"mushroom":mushroomWeight},"farming_weight":{"farming_weight":farming_weight,"farming_level":farming_level},"minions":{"minion_weight":minion_weight,"minions":minions},"jacub":{"jacub_weight":jacub_weight,"jacub_perks":jacub_perks},"gold":{"golds":gold,"gold_weight":gold_weight}}]
-    else:
-        return [0,"Error: No player found. Please try again later or contact the developer at CosmicCrow#6355."]
+    return response['highest']['farming']['weight']
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(autoroles(bot))
